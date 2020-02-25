6d06c3a483bcf9fc86e4a8a90648777eb1219e47
unknown <jofuentesle@uoc.edu>
2020-02-25 09:46:37 +0100

    update

---
+++ src/app/shared/services/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  SERVER_URL = 'http://localhost:4200/api/';
+
+  constructor( private httpClient: HttpClient) { }
+
+  public getUsers() {
+    return this.httpClient.get(this.SERVER_URL + 'users');
+  }
+}
---
+++ src/app/views/signin/signin.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/shared/services/data.service';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.scss']
+})
+export class SigninComponent implements OnInit {
+
+  users: any[] = [];
+
+
+  constructor( private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.getUsers().subscribe((data: any[]) => {
+      console.log(data);
+      this.users = data;
+    });
+  }
+
+}
